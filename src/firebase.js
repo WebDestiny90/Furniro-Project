9c602b6c36edc0720a374621161ed4092a261cec
WebDestiny90 <azeagent@gmail.com>
2025-05-29 00:42:41 +0400

    fixed api key

--- src/firebase.js
+++ src/firebase.js
@@ -11,13 +11,6 @@ const firebaseConfig = {
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
-  // apiKey: "AIzaSyB3ZO4MZzeasf_M1nodMsyKGrAaVWd4NrI",
-  // authDomain: "furniro-account.firebaseapp.com",
-  // projectId: "furniro-account",
-  // storageBucket: "furniro-account.firebasestorage.app",
-  // messagingSenderId: "575825340755",
-  // appId: "1:575825340755:web:3eae216dd76ec1220699a6",
-  // measurementId: "G-GM0N8DK9VL"
 };
 
 const app = initializeApp(firebaseConfig);
